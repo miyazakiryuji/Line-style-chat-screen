c491e99b9ed0c96ee8872fb0051669d644b8ee4d
宮﨑竜治 <>
2025-01-27 18:31:31 +0900

    新規プロジェクトの作成

---
+++ ChatViewWithSwiftUI/ChatViewWithSwiftUITests/ChatViewWithSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewWithSwiftUITests.swift
+//  ChatViewWithSwiftUITests
+//
+//  Created by 宮﨑竜治 on 2025/01/27.
+//
+
+import Testing
+@testable import ChatViewWithSwiftUI
+
+struct ChatViewWithSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
